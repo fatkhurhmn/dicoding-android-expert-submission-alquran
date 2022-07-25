84225a38b8226e74201f603d8eff6382bf8218c6
Muhammad Fatkhu Rohman <fatkhurohman019@gmail.com>
2022-07-25 22:26:38 +0700

    settings project and implement UI for main menu

--- app/src/main/java/com/muffarproject/alquran/main/MainActivity.kt
+++ app/src/main/java/com/muffarproject/alquran/main/MainActivity.kt
@@ -1,8 +1,12 @@
-package com.muffarproject.alquran
+package com.muffarproject.alquran.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.muffarproject.alquran.R
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.HiltAndroidApp
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
