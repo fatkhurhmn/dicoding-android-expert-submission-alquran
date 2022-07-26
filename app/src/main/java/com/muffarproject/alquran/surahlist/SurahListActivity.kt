5d2d3bac06d320d2eaef27b770462432e402bc7b
Muhammad Fatkhu Rohman <fatkhurohman019@gmail.com>
2022-07-26 11:24:07 +0700

    create UI for surah_item.xml

---
+++ app/src/main/java/com/muffarproject/alquran/surahlist/SurahListActivity.kt
@@ -0,0 +1,12 @@
+package com.muffarproject.alquran.surahlist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.muffarproject.alquran.R
+
+class SurahListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_surah)
+    }
+}
