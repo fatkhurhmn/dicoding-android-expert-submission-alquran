23af1535286becdf653484e21f96e9e4eb55ba39
Muhammad Fatkhu Rohman <fatkhurohman019@gmail.com>
2022-08-03 15:47:07 +0700

    implement certificate pinning for server connection

--- core/src/main/java/com/muffarproject/core/di/NetworkModule.kt
+++ core/src/main/java/com/muffarproject/core/di/NetworkModule.kt
@@ -6,6 +6,7 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -18,6 +19,13 @@ class NetworkModule {
 
     @Provides
     fun provideOkhttpClient(): OkHttpClient {
+        val hostname = "api-alquranid.herokuapp.com"
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname, "sha256/2ZRL7V7YvOKNyhFK2GLbFqLGztyfZjQK9SD4O65knX0=")
+            .add(hostname, "sha256/JSMzqOOrtyOT1kmau6zKhgT676hGgczD5VMdRMyJZFA=")
+            .add(hostname, "sha256/++MBgDH5WGvL9Bcn5Be30cRcL0f5O+NyoXuWtQdX1aI=")
+            .add(hostname, "sha256/KwccWaCgrnaw6tsrrSO61FgLacNgG2MMLq8GE6+oP5I=")
+            .build()
         return OkHttpClient.Builder()
             .addInterceptor(
                 HttpLoggingInterceptor().setLevel(
@@ -30,6 +38,7 @@ class NetworkModule {
             )
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
     }
 
